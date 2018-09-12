9742d3227ed55e1fde7b8f06d0d67e545f945bcb
Laura Corssac <lauracorssac@hotmail.com>
2018-09-11 21:22:41 -0300

    bug fixed, images added. Working version!

--- TrabFPI/ViewController.swift
+++ TrabFPI/ViewController.swift
@@ -21,58 +21,77 @@ class ViewController: UIViewController {
     var bytesPerRow: Int!
     let colorSpace = CGColorSpaceCreateDeviceRGB()
     var bitmapInfo: UInt32!
+    var rawData: UnsafeMutablePointer<UInt32>!
     
-    @IBAction func saveButtonPressed(_ sender: UIButton) {
-        let outContext = CGContext(data: pixels.baseAddress, width: width, height: height, bitsPerComponent: bitsPerComponent, bytesPerRow: bytesPerRow, space: colorSpace,bitmapInfo: bitmapInfo, releaseCallback: nil, releaseInfo: nil)
-        
-        let outImage = UIImage(cgImage: outContext!.makeImage()!)
-        
-        //let paths = NSSearchPathForDirectoriesInDomains(.documentDirectory, .userDomainMask, true)
+    func save(data: Data?, name: String) {
         let urls = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
-        let data = UIImageJPEGRepresentation(outImage, 1)
         guard let imageData = data else {
             print("no image data")
             return
         }
         do {
-            try imageData.write(to: urls.first!.appendingPathComponent("copy.jpg"))
+            try imageData.write(to: urls.first!.appendingPathComponent(name))
+            print("imagem salva com sucesso")
         } catch let error {
-           print("unable to save", error.localizedDescription)
+            print("unable to save", error.localizedDescription)
         }
-        
     }
-    override func viewDidLoad() {
-        super.viewDidLoad()
+    
+    @IBAction func saveButtonPressed(_ sender: UIButton) {
+        self.view.endEditing(true)
+        let outImage = copyImage()
+        let data = UIImageJPEGRepresentation(outImage, 1)
+        save(data: data, name: "copy.jpeg")
+    }
+    func configureUI() {
         textField.layer.borderWidth = 2
         textField.layer.cornerRadius = 3
         textField.layer.borderColor = UIColor.black.cgColor
         textField.delegate = self
-        let image = UIImage(named: "gigi2.jpg")
+        imageVIew.contentMode = .center
+        outImageView.contentMode = .center
+    }
+    func loadInitialImage() {
+        let image = UIImage(named: "Gramado_72k")
         height = Int((image?.size.height)!)
         width = Int((image?.size.width)!)
         bytesPerRow = 4 * width // 4 para RGBA
-   
+        
         //cada pixel 32 bits. alocamos array de width * height pixels
-        let rawData = UnsafeMutablePointer<UInt32>.allocate(capacity: width * height)
+        rawData = UnsafeMutablePointer<UInt32>.allocate(capacity: width * height)
         
         bitmapInfo = CGBitmapInfo.byteOrder32Big.rawValue | CGImageAlphaInfo.premultipliedLast.rawValue
         let CGPointZero = CGPoint(x: 0, y: 0)
         let rect = CGRect(origin: CGPointZero, size: (image?.size)!)
-
-        let imageContext = CGContext(data: rawData, width: width, height: height, bitsPerComponent: bitsPerComponent, bytesPerRow: bytesPerRow, space: colorSpace, bitmapInfo: bitmapInfo)
+        
+        let imageContext = CGContext(data: rawData,
+                                     width: width, height: height,
+                                     bitsPerComponent: bitsPerComponent,
+                                     bytesPerRow: bytesPerRow,
+                                     space: colorSpace,
+                                     bitmapInfo: bitmapInfo)
         
         imageContext?.draw(image!.cgImage!, in: rect)
         
         pixels = UnsafeMutableBufferPointer<UInt32>(start: rawData, count: width * height)
         
-        let outContext = CGContext(data: pixels.baseAddress, width: width, height: height, bitsPerComponent: bitsPerComponent,bytesPerRow: bytesPerRow,space: colorSpace,bitmapInfo: bitmapInfo,releaseCallback: nil,releaseInfo: nil)
-        
+        let outContext = CGContext(data: pixels.baseAddress,
+                                   width: width, height: height,
+                                   bitsPerComponent: bitsPerComponent,
+                                   bytesPerRow: bytesPerRow,
+                                   space: colorSpace,
+                                   bitmapInfo:bitmapInfo,releaseCallback: nil,
+                                   releaseInfo: nil)
         
         let outImage = UIImage(cgImage: outContext!.makeImage()!)
         self.imageVIew.image = outImage
-  
     }
-    @IBAction func buttonPressed(_ sender: UIButton) {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        configureUI()
+        loadInitialImage()
+    }
+    @IBAction func flipVerticalButtonPressed(_ sender: UIButton) {
         let rawData = UnsafeMutablePointer<UInt32>.allocate(capacity: width * height)
         let pixelsCopy = UnsafeMutableBufferPointer<UInt32>(start: rawData, count: width * height)
         for (i, _) in pixelsCopy.enumerated() {
@@ -88,8 +107,11 @@ class ViewController: UIViewController {
         pixels = pixelsCopy
         let outImage = UIImage(cgImage: outContext!.makeImage()!)
         self.outImageView.image = outImage
+        let data = UIImageJPEGRepresentation(outImage, 1.0)
+        save(data: data, name: "copy_vertical.jpeg")
+        //free(self.rawData)
+        //self.rawData = rawData
     }
-    
     
     @IBAction func flipHorizontalButtonPressed(_ sender: UIButton) {
         
@@ -107,23 +129,47 @@ class ViewController: UIViewController {
         
         pixels = pixelsCopy
         let outImage = UIImage(cgImage: outContext!.makeImage()!)
+        let data = UIImageJPEGRepresentation(outImage, 1.0)
+        save(data: data, name: "copy_horizontal.jpeg")
         self.outImageView.image = outImage
+        //free(self.rawData)
+        //self.rawData = rawData
         
     }
     
+    func copyImage() -> UIImage {
+        let rawData = UnsafeMutablePointer<UInt32>.allocate(capacity: width * height)
+        let pixelsCopy = UnsafeMutableBufferPointer<UInt32>(start: rawData, count: width * height)
+        for (i, _) in pixelsCopy.enumerated() {
+            pixelsCopy[i] = pixels[i]
+        }
+        let outContext = CGContext(data: pixelsCopy.baseAddress, width: width, height: height, bitsPerComponent: bitsPerComponent, bytesPerRow: bytesPerRow, space: colorSpace,bitmapInfo: bitmapInfo, releaseCallback: nil, releaseInfo: nil)
+        
+        let outImage = UIImage(cgImage: outContext!.makeImage()!)
+        pixels = pixelsCopy
+        //free(self.rawData)
+        return outImage
+    }
+    
+    
+    
+    @IBAction func copyButtonPressed(_ sender: Any) {
+        
+        self.view.endEditing(true)
+        self.outImageView.image = copyImage()
+    }
     @IBAction func grayScaleButtonPressed(_ sender: UIButton) {
         
         let rawData = UnsafeMutablePointer<UInt32>.allocate(capacity: width * height)
         let pixelsCopy = UnsafeMutableBufferPointer<UInt32>(start: rawData, count: width * height)
         for (i, _) in pixelsCopy.enumerated() {
             
-            
             let pixel = pixels[i]
         
             let byte0 = UInt8(pixel & 0x000000FF)
             let byte1 = UInt8((pixel & 0x0000FF00) >> 8)
             let byte2 = UInt8((pixel & 0x00FF0000) >> 16)
-            let byte3 = UInt8((pixel & 0xFF000000) >> 24) // alpha vem primeiro???
+            let byte3 = UInt8((pixel & 0xFF000000) >> 24) //alpha
             let red = Double(byte0)
             let green = Double(byte1)
             let blue = Double(byte2)
@@ -134,11 +180,23 @@ class ViewController: UIViewController {
             
             pixelsCopy[i] = int32
         }
-        let outContext = CGContext(data: pixelsCopy.baseAddress, width: width, height: height, bitsPerComponent: bitsPerComponent, bytesPerRow: bytesPerRow, space: colorSpace,bitmapInfo: bitmapInfo, releaseCallback: nil, releaseInfo: nil)
+        let outContext = CGContext(data: pixelsCopy.baseAddress,
+                                   width: width, height: height,
+                                   bitsPerComponent: bitsPerComponent,
+                                   bytesPerRow: bytesPerRow,
+                                   space: colorSpace,
+                                   bitmapInfo: bitmapInfo,
+                                   releaseCallback: nil,
+                                   releaseInfo: nil)
         
         pixels = pixelsCopy
         let outImage = UIImage(cgImage: outContext!.makeImage()!)
         self.outImageView.image = outImage
+        
+        let data = UIImageJPEGRepresentation(outImage, 1.0)
+        save(data: data, name: "copy_gray.jpeg")
+        //free(self.rawData)
+        //self.rawData = rawData
         
     }
     
@@ -149,7 +207,7 @@ extension ViewController: UITextFieldDelegate {
         guard let _ = textField.text, let number = Int(textField.text!) else {
             return false
         }
-        if number > 0 && number <= 256 {
+        if number > 2 && number <= 256 {
             textField.layer.borderColor = UIColor.black.cgColor
             textField.resignFirstResponder()
             return true
@@ -159,16 +217,17 @@ extension ViewController: UITextFieldDelegate {
         }
     }
     func textFieldDidEndEditing(_ textField: UITextField, reason: UITextFieldDidEndEditingReason) {
+        guard let shadesNumber = Int(textField.text!) else {
+            return
+        }
         let rawData = UnsafeMutablePointer<UInt32>.allocate(capacity: width * height)
         let pixelsCopy = UnsafeMutableBufferPointer<UInt32>(start: rawData, count: width * height)
         for (i, _) in pixelsCopy.enumerated() {
             
             let pixel = pixels[i]
-            
             let byte0 = UInt8(pixel & 0x000000FF)
-            let byte3 = UInt8((pixel & 0xFF000000) >> 24) // alpha vem primeiro???
+            let byte3 = UInt8((pixel & 0xFF000000) >> 24) // alpha
             let shade = Int(byte0)
-            let shadesNumber = Int(textField.text!)!
             var newShade: Int
             
             if shadesNumber <= 1 {
@@ -192,21 +251,18 @@ extension ViewController: UITextFieldDelegate {
               
             }
             let int8 = UInt8.init(newShade)
-            if int8 != shade {
-                 print("new", newShade, "old", shade)
-            }
             let int32 = UInt32(byte3) << 24 | UInt32(int8) << 16 | UInt32(int8) << 8 | UInt32(int8)
             
             pixelsCopy[i] = int32
         }
         let outContext = CGContext(data: pixelsCopy.baseAddress, width: width, height: height, bitsPerComponent: bitsPerComponent, bytesPerRow: bytesPerRow, space: colorSpace,bitmapInfo: bitmapInfo, releaseCallback: nil, releaseInfo: nil)
         
-        
         let outImage = UIImage(cgImage: outContext!.makeImage()!)
+        let data = UIImageJPEGRepresentation(outImage, 1.0)
+        save(data: data, name: "copy_gray_\(shadesNumber).jpeg")
         self.outImageView.image = outImage
     }
     func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
-        
         return true
     }
 }
